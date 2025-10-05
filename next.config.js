03ab807c35132fd1264fe26ba0137472bf32cb01
Kunal_©oder <166412409+Kunal28122004@users.noreply.github.com>
2025-10-05 19:37:11 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // ensures static export
+  basePath: '/3d-card', // required for GitHub Pages
+};
+
+module.exports = nextConfig;
